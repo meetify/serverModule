55b6aef32bccbd9c242440ca21a7b590967618a7
kr3v <d.baynak@protonmail.ch>
2016-09-10 12:40:41 +0300

    place changes, name addition going to parse places in meetify/server

--- src/serverModule/data/Place.kt
+++ src/serverModule/data/Place.kt
@@ -3,7 +3,6 @@ package serverModule.data
 /**
  * Created by kr3v on 05.09.2016.
  */
-class Place(val name:String,
-            val type:String,
-            val location: Location) {
-}
+data class Place(val name: Name,
+                 val type: String?,
+                 val location: Location)
